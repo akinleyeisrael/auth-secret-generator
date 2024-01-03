5cb3b889e9c6fd3c0ae540f6858a29a90b1cd4c4
israel <akinleyeisrael12@gmail.com>
2024-01-03 14:38:07 -0500

    make app responsive

--- app/page.tsx
+++ app/page.tsx
@@ -24,13 +24,13 @@ export default function Home() {
   return (
     <div>
       <div className="flex flex-col items-center mx-auto space-y-6 mt-32 px-10 sm:mt-52">
-        <h1 className="text-5xl leading-10 tracking-tighter mb-4 font-bold">
+        <h1 className="text-5xl leading-10 tracking-tighter mb-4 font-bold  whitespace-break-spaces">
           Auth secret generator for next js apps
         </h1>
         <Button onClick={generateOpenssl} variant={"secondary"}>Generate</Button>
         <div className="flex space-x-2 text-primary-foreground relative">
           <div className="bg-primary border rounded-lg px-6 h-20 w-[20rem] md:w-[40rem] justify-center">
-            <p className="max-w-[15rem] my-6 break-before-all break-words text-xs sm:max-w-lg sm:my-8 md:max-w-[30rem]">{key}</p>
+            <p placeholder="Auth gen" className="max-w-[15rem] my-6 break-before-all break-words text-xs sm:max-w-lg sm:my-8 md:max-w-[30rem]">{key}</p>
           </div>
           <CopyIcon onClick={copyKey} className="absolute end-6 m-8 hover:cursor-pointer" />
         </div>
